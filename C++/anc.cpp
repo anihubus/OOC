b29968f860be0a401af209ac169d18a20f8b3bf9
anihubus <sujalkrvishwa@gmail.com>
2026-08-07 10:43:10 +0530

    area and circumference of circle

---
+++ C++/anc.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float r;
+    cout<<"Enter the radius of the circle: ";
+    cin>>r;
+    cout<<"Area of the circle is "<<3.14*r*r;
+    return 0;
+}
