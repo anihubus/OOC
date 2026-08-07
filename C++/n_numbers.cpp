164a73673bb4feaabad6a26a2f0c9049d46f47ac
anihubus <sujalkrvishwa@gmail.com>
2026-08-07 10:57:02 +0530

    print first n numbers

---
+++ C++/n_numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,i=1;
+    cout<<"Enter the number you want to print: ";
+    cin>>a;
+    while(i<=a) {
+        cout<<i<<endl;
+        i++;
+    }
+    return 0;
+}
